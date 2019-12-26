87254a0582757ea5861d50d608d5d9651a69b56c
Michael Ip <michaelip@Michaels-MacBook-Air.local>
2019-12-26 15:26:09 -0500

    added current move visuals

--- script.js
+++ script.js
@@ -5,6 +5,13 @@
  * Win or Draw - prompted for replay
  * Keep Score
  * 
+ * Currently on replay the window is just reloaded. I know this is a cheating way to implement playability
+ * But I overcome this problem with storing the running score locally.
+ * 
+ * A better way to implement this would be having a function that resets boxValue and removes played squares. This shoudln't be too hard with an array loops. Will implement this next.
+ * 
+ * Also will clean up the check winner code. It's a bit sloppy right now.
+ * 
  * winner logic: only 8 winning combinations (3 in a row)
  * - 3 across [0,1,2], [3,4,5], [6,7,8]
  * - 3 up and down [0,3,6], [1,4,7], [2,5,8]
@@ -21,9 +28,10 @@
 let gameBoard = document.querySelectorAll('.ticbox'); // sets up the gameboard
 let playerMove = firstMove(); // determines whose turn it is false = p1, true = p2
 let boxCounter = 0; // checks to see if all boxes played
-let score = []; // score tracker currently unused
+// let score = []; // score tracker currently unused
 let replay = false;
-let totalScores = getScores();
+let totalScores = getScores(); // locally stored running scores
+let currentPlayer = document.getElementById('playerturn'); // current player's turn
 
 gameBoard.forEach(element =>{ // adds click event listener
     element.addEventListener('click',currentMove);
@@ -35,8 +43,8 @@ gameSplash(); // game starts here
 // playerSelect();
 
 
-function currentMove(){ // places current marker in box
-    if (playerMove){
+function currentMove(){ // places current marker in box - triggered by eventListener
+    if (playerMove){ // play morty if "true"
         if (this.hasClicked){
             alert("You've already played this box");
         } else {
@@ -50,7 +58,7 @@ function currentMove(){ // places current marker in box
         // console.log(boxCounter);
         this.hasClicked=true;
         }
-    } else {
+    } else { // play rick if "false"
         if (this.hasClicked){
             alert("You've already played this box");
         } else {
@@ -80,18 +88,14 @@ function firstMove(){ //determines who goes first
 }
 
 function nextTurn(){ //who goes next
+    // currentPlayer.style.display = "inherit";
+    currentPlayer.innerText = `Player${!playerMove ? 'Morty' : 'Rick'}'s move`;
     return !playerMove
 }
 
 function checkWinner(winner){ // function to check for a winner - hard coded for now
-    if ((gameBoard[0].boxValue==gameBoard[1].boxValue&&gameBoard[1].boxValue==gameBoard[2].boxValue)||
-       (gameBoard[3].boxValue==gameBoard[4].boxValue&&gameBoard[4].boxValue==gameBoard[5].boxValue)||
-       (gameBoard[6].boxValue==gameBoard[7].boxValue&&gameBoard[7].boxValue==gameBoard[8].boxValue)||
-       (gameBoard[0].boxValue==gameBoard[3].boxValue&&gameBoard[3].boxValue==gameBoard[6].boxValue)||
-       (gameBoard[1].boxValue==gameBoard[4].boxValue&&gameBoard[4].boxValue==gameBoard[7].boxValue)||
-       (gameBoard[2].boxValue==gameBoard[5].boxValue&&gameBoard[5].boxValue==gameBoard[8].boxValue)||
-       (gameBoard[0].boxValue==gameBoard[4].boxValue&&gameBoard[4].boxValue==gameBoard[8].boxValue)||
-       (gameBoard[2].boxValue==gameBoard[4].boxValue&&gameBoard[4].boxValue==gameBoard[6].boxValue)){
+    if (winningCombo()){
+        currentPlayer.style.display = "none";
         if(winner=='Rick'){ // updates scores
             updateScores(1,0,1);
         } else if(winner=='Morty'){
@@ -102,11 +106,12 @@ function checkWinner(winner){ // function to check for a winner - hard coded for
         After ${totalScores[2]} games, the score is Rick: ${totalScores[0]} Morty: ${totalScores[1]}\n Would you like to play again?`);}, 2000); 
         setTimeout(()=>{
         if (replay){ // needs to get rid of time out for this to work
-            console.log('start new game');
+            // console.log('start new game');
             location.reload(); // reloads the game
         }}, 2000);
 
        } else if (boxCounter==9){
+        currentPlayer.style.display = "none";
         setTimeout(()=>{document.getElementById('whatever').play();}, 1800);
         setTimeout(()=>{replay = confirm(`It's a draw! Would you like to play again?`);}, 2000);
         updateScores(0,0,1);
@@ -119,16 +124,6 @@ function checkWinner(winner){ // function to check for a winner - hard coded for
         } else {
            console.log("no winner");
         }
-}
-
-function isEquals(val1, val2, val3){ // function to check 3 values, tbc...
-/**************
- * 0 * 3 * 6 *
- * 1 * 4 * 7 *
- * 2 * 5 * 8 *
- *************
- * Every time a spot is played check the up/down, left/right, diagonal
- */
 }
 
 function gameSplash(){ // function to display splash
@@ -152,7 +147,7 @@ function playerSelect(){ //visually shows players who goes first
 
     let selector = document.querySelector("#armselector");
     let playerName;
-    if(firstMove()){
+    if(playerMove){
         setTimeout(()=>{
             selector.style.transform = 'rotate(20.40turn)'; // morty
             playerName = 'Morty';
@@ -178,8 +173,6 @@ function playerSelect(){ //visually shows players who goes first
 
 }
 
-
-
 function gameStart(){ // show me what you got! (display cromulon increase size massively)
     let gameStart = document.querySelector('main');
     let visibleLoad = document.querySelector('#cromulonload'); // loads cromulon
@@ -196,6 +189,9 @@ function gameStart(){ // show me what you got! (display cromulon increase size m
 
     setTimeout(()=>{
         visibleLoad.remove(); // deletes cromulon
+        // let currentPlayer = document.getElementById('playerturn');
+        currentPlayer.style.display = "inherit";
+        currentPlayer.innerText = `Player${playerMove ? 'Morty' : 'Rick'}'s move`;
         gameStart.style.display="flex"; // display flex makes gameboard appear
     },3000);
 }
@@ -232,4 +228,36 @@ function updateScores(rick,morty,games){ //updates after every game
     console.log(totalScores);
 }
 
+function winningCombo(){ // function to check 3 values, tbc...
+    /**************
+     * 0 * 3 * 6 *
+     * 1 * 4 * 7 *
+     * 2 * 5 * 8 *
+     *************
+     * Every time a spot is played check the up/down, left/right, diagonal
+    checkUp -> 0-3-6, 1-4-7, 2-5-8
+    checkAcross -> 0-1-2, 3-4-5, 6-7-8
+    checkDiagonal -> 0-4-8, 2-4-6
 
+    Currently checkWinner runs through all the combinations -> not very efficient
+     */
+    //gameBoard - global array storing all boxValue of played squares
+   
+
+    if (boxCounter<5){  // if boxCounter < 5 - not possible there is a winner (min 5)
+        return false; 
+    }
+
+    if ((gameBoard[0].boxValue==gameBoard[1].boxValue&&gameBoard[1].boxValue==gameBoard[2].boxValue)||
+       (gameBoard[3].boxValue==gameBoard[4].boxValue&&gameBoard[4].boxValue==gameBoard[5].boxValue)||
+       (gameBoard[6].boxValue==gameBoard[7].boxValue&&gameBoard[7].boxValue==gameBoard[8].boxValue)||
+       (gameBoard[0].boxValue==gameBoard[3].boxValue&&gameBoard[3].boxValue==gameBoard[6].boxValue)||
+       (gameBoard[1].boxValue==gameBoard[4].boxValue&&gameBoard[4].boxValue==gameBoard[7].boxValue)||
+       (gameBoard[2].boxValue==gameBoard[5].boxValue&&gameBoard[5].boxValue==gameBoard[8].boxValue)||
+       (gameBoard[0].boxValue==gameBoard[4].boxValue&&gameBoard[4].boxValue==gameBoard[8].boxValue)||
+       (gameBoard[2].boxValue==gameBoard[4].boxValue&&gameBoard[4].boxValue==gameBoard[6].boxValue)){
+        // checks for all winning combinations 
+        return true;
+    }
+    return false; // no winner
+}
